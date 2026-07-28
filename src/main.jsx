643f2a6429a5733dce85a3f0a7a8ba8d7ccbdd01
pranit1096 <pranitpatil1011@gmail.com>
2026-07-28 13:33:43 +0530

    Add main component rendering in main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import TrustLayerDemo from "./TrustLayerDemo.jsx";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <TrustLayerDemo />
+  </React.StrictMode>
+);
